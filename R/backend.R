2993cf7b7c1b7eb680692d098d4ab4999cf75021
Martin Morgan <mtmorgan@fhcrc.org>
2015-05-13 17:17:33 -0700

    serial and socket backends

--- R/backend.R
+++ R/backend.R
@@ -19,39 +19,59 @@
 
 ## 
 
-backend <-
-    function(spec, type, ..., outfile="")
+.backend_class <-
+    function(x, type)
 {
-    if (missing(spec) && missing(type)) {
-        if (.backend_exists())
-            return(.backend_get_current())
-    }
-    if (missing(spec))
-        spec <- as.integer(ceiling(0.75 * parallel::detectCores()))
-    if (missing(type))
-        type <- "socket"                  # FIXME -- platform-specific
-    stopifnot(length(type) == 1L, type %in% c("socket"))
-    cl <- switch(type, socket={
-        invalid <- c("snowlib", "scriptdir")
-        if (any(idx <- invalid %in% names(list(...))))
-            stop("argument ", paste(sQuote(invalid[idx]), collapse=", "),
-                 "not allowed for SOCK cluster")
-        snowlib <- system.file(package="strmr", "node")
-        scriptdir <- file.path(snowlib, "snow")
-        makeSOCKcluster(spec, snowlib=snowlib, scriptdir=scriptdir, ...,
-                        outfile=outfile)
-    }, {
-        stop("unknown backend type '", type, "'")
-    })
-    class(cl) <- c(sprintf("strm_%s", type), "strm_backend", class(cl))
+    c(sprintf("strm_%s", type), "strm_backend", class(cl))
+}
+
+backend <-
+    function(spec, type, ...)
+{
+    if (missing(spec) && missing(type) && .backend_exists())
+        .backend_get_current()
+    ## FIXME: platform-specific default
+    backend_socket(spec, ...)
+}
+
+close.strm_backend <- function(con, ...) {}
+
+print.strm_backend <- function(x, ...)
+    cat(class(x)[1], "backend\n")
+
+## serial
+
+backend_serial <-
+    function(...)
+{
+    cl <- list()
+    class(cl) <- .backend_class(cl, "serial")
     .backend_set_current(cl)
 }
 
-close.strm_backend <- function(con, ...) {
+## socket
+
+backend_socket <-
+    function(spec, ...)
+{
+    if (missing(spec))
+        spec <- as.integer(ceiling(0.75 * parallel::detectCores()))
+    invalid <- c("snowlib", "scriptdir")
+    if (any(idx <- invalid %in% names(list(...))))
+        stop("argument ", paste(sQuote(invalid[idx]), collapse=", "),
+             "not allowed for SOCK cluster")
+    snowlib <- system.file(package="strmr", "node")
+    scriptdir <- file.path(snowlib, "snow")
+    cl <- makeSOCKcluster(spec, snowlib=snowlib, scriptdir=scriptdir, ...)
+    class(cl) <- .backend_class(cl, "socket")
+    .backend_set_current(cl)
+}
+
+close.strm_socket <- function(con, ...) {
     stopCluster(con)
 }
 
-print.strm_backend <- function(x, ...) {
+print.strm_socket <- function(x, ...) {
     hosts <- table(vapply(x, `[[`, character(1), "host"))
     cat(sprintf("%s backend of length %d", class(x)[[1]], length(x)),
         paste(sprintf("\n  %s (x%d)", names(hosts), as.vector(hosts)),
