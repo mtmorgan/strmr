f0520c506e7ff1a0515fbb2a5a5a73c180782ce6
Martin Morgan <mtmorgan@fhcrc.org>
2015-05-13 18:10:24 -0700

    reorder status codes

--- R/strm_yield_status.R
+++ R/strm_yield_status.R
@@ -1,7 +1,7 @@
 ## yield_status
 
 .strm_yield_status <- function(status, condition=NULL) {
-    value <- list(pending=1L, done=2L, error=-1L)
+    value <- list(error=-1L, pending=1L, done=2L)
     stopifnot(status %in% names(value))
     class <- c(sprintf("strm_%s", status), "strm_yield_status", "strmr")
     structure(list(status=value[status], condition=condition), class=class)
